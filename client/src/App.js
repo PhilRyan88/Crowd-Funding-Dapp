5e7515bf35d618f1607333ad4b804ef7f04b4634
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-08-29 21:57:38 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ const App = () => {
       <h1>Crowd Funding Dapp</h1>
       <br />
       <br />
+      <span>Raise a funding</span>
+      <button type="submit" onClick={""} />
     </div>
   );
 };
